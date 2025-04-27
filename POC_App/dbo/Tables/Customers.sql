5d05242a11d7703162636a237ad92ae620508672
yorik79 <yurig@madeiradata.com>
2025-04-27 21:13:11 +0300

    v1

---
+++ POC_App/dbo/Tables/Customers.sql
@@ -0,0 +1,7 @@
+﻿CREATE TABLE [dbo].[Customers] (
+    [id]          INT       IDENTITY (1, 1) NOT NULL,
+    [user_id]     INT       NULL,
+    [customer_id] NCHAR (9) NULL,
+    [createdon]   DATETIME  NULL
+);
+
---
+++ POC_App/dbo/Views/CustomersToUsers.sql
@@ -0,0 +1,7 @@
+﻿
+--CREATE TABLE dbo.Customers (id int identity(1,1), user_id int, customer_id nchar(9), createdon datetime);
+--GO
+CREATE VIEW [dbo].[CustomersToUsers] 
+AS
+SELECT Customers.id,Users.[user_id], 'App2' AS SourceType FROM dbo.Customers INNER JOIN [$(POC_MeitavUsers)].dbo.Users
+ON Customers.[user_id] = Users.[user_id]
