ee8fc9b16e8f9d18d9a2d47d9bb52b9ee7639be5
yorik79 <yurig@madeiradata.com>
2025-04-28 09:13:02 +0300

    v3

--- POC_App/dbo/Tables/Customers.sql
+++
@@ -1,7 +0,0 @@
-﻿CREATE TABLE [dbo].[Customers] (
-    [id]          INT       IDENTITY (1, 1) NOT NULL,
-    [user_id]     INT       NULL,
-    [customer_id] NCHAR (9) NULL,
-    [createdon]   DATETIME  NULL
-);
-
